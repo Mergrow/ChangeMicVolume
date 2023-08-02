a24c5e70c5554696053ccc8ebc469ac7d3d373d4
Mergrow <reversivelbatle@gmail.com>
2023-08-02 11:14:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using AudioSwitcher.AudioApi.CoreAudio;
+namespace ChangeMicVolume // Note: actual namespace depends on the project name.
+{
+    internal class ChangeMicVolume
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CoreAudioDevice DefaultCaptureDevice = new CoreAudioController().DefaultCaptureDevice;
+                Console.WriteLine(DefaultCaptureDevice.FullName + " current volume of " + "is: " + DefaultCaptureDevice.Volume);
+                while (true)
+                {
+                    Console.WriteLine("Enter the desired volume: ");
+                    int volume = Convert.ToInt32(Console.ReadLine());
+
+                    void FixVolume(int volume)
+                    {
+
+                        DefaultCaptureDevice.Volume = volume;
+                        Console.WriteLine(DefaultCaptureDevice.FullName + " volume has been changed to: " + volume);
+                    }
+
+
+                    FixVolume(volume);
+                }
+            }
+        }
+    }
+}
